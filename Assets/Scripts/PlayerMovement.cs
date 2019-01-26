c83eab815dd6afb98d1d377c134962dcc55e74b9
Excilyano <agahnim13@gmail.com>
2019-01-26 03:23:18 +0100

    proto

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+        transform.Translate (movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Respawn") {
+            Destroy(other.gameObject, 20f);
+            Destroy(other);
+            LevelManager.SpawnNextPrefab(other.gameObject.transform.position);
+        }
+        if(other.gameObject.tag == "Finish") {
+            Debug.Log("T ES MORT !");
+        }
+    }
+}
---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3 (0.0f, -1f, 0.0f);
+
+        transform.Translate (movement * speed);
+    }
+}
