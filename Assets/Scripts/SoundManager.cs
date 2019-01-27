1fbf708897fc27a99599d54728bf9f507773e2c6
Excilyano <agahnim13@gmail.com>
2019-01-27 16:19:26 +0100

    Final

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -13,22 +13,27 @@ public class SoundManager : MonoBehaviour
 
     public AudioSource vroum;
     public static AudioSource svroum;
-    public AudioClip vroumClip;
+    public AudioClip[] vroumClip;
+    public static AudioClip[] svroumClip;
+
+    public AudioSource woof;
+    public AudioClip[] woofClip;
 
     // Start is called before the first frame update
     void Start()
     {
         bass.clip = bassClip;
         bass2.clip = bassClip;
-        vroum.clip = vroumClip;
         tint.volume = .5f;
         bass.volume = .5f;
         bass2.volume = .5f;
         bass.Play();
         StartCoroutine(loopBass());
         StartCoroutine(randomizeTint());
+        StartCoroutine(randomizeWoof());
 
         svroum = vroum;
+        svroumClip = vroumClip;
     }
 
     public IEnumerator loopBass() {
@@ -46,8 +51,19 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    public IEnumerator randomizeWoof() {
+        while(true) {
+            float random = Random.Range(0f, 20f);
+            yield return new WaitForSeconds(random);
+            int rand = Random.Range(0, woofClip.Length);
+            woof.clip = woofClip[rand];
+            woof.Play();
+        }
+    }
+
     public static void Vroum() {
         svroum.volume = 1f;
+        svroum.clip = svroumClip[Random.Range(0, svroumClip.Length)];
         svroum.Play();
     }
 }
